a38d3dadee8c5af292a4a18776febb2f74c902fb
Esteban Morales <esteban@Estebans-MacBook-Pro.local>
2020-09-23 17:50:03 -0500

    Update

--- Nabla Looper/Arranged Mode/Nabla Looper Arranged Settings.lua
+++ Nabla Looper/Arranged Mode/Nabla Looper Arranged Settings.lua
@@ -15,15 +15,6 @@ else
 	scriptPath = reaper.GetResourcePath().."/Scripts/Nabla Tools/Scythe for Nabla/library/"
 end
 
-local libPathNabla = reaper.GetExtState("Scythe v3 for Nabla", "libPathNabla")
-
-if not libPathNabla or libPathNabla == "" then
-
-  reaper.SetExtState("Scythe v3 for Nabla", "libPathNabla", scriptPath, true)
-
-  libPathNabla = scriptPath
-
-end
 dofile(script_path .. "Nabla Looper Arranged Settings"..".dat")
 
 
