248fa64e93105733e31715795c65aa4c5c541954
Esteban Morales <esteban@nablatools.com>
2020-10-08 00:53:06 -0500

    Create Toggle Record Item

---
+++ Nabla Looper/Arranged Mode/Nabla Looper Arranged Items (Toggle Record Mute).lua
@@ -0,0 +1,11 @@
+--====================================================================== 
+--[[ 
+* ReaScript Name: Nabla Looper Arranged Items (Toggle Record Mute)
+* Version: 0.2.0
+* Author: Esteban Morales
+* Author URI: http://forum.cockos.com/member.php?u=133640 
+--]] 
+--====================================================================== 
+local info   = debug.getinfo(1,'S');
+script_path  = info.source:match[[^@?(.*[\/])[^\/]-$]]
+dofile(script_path .. "Nabla Looper Arranged Items (Toggle Record Mute).dat")
