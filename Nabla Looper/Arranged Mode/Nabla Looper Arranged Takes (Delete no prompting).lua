2314b6bf707111a70a4cc698081dff1b52840107
Esteban Morales <esteban@nablatools.com>
2020-10-09 17:36:57 -0500

    Add Delete no prompting

---
+++ Nabla Looper/Arranged Mode/Nabla Looper Arranged Takes (Delete no prompting).lua
@@ -0,0 +1,12 @@
+--====================================================================== 
+--[[ 
+* ReaScript Name: Nabla Looper Arranged Takes (Delete no prompting)
+* Version: 0.2.5
+* Author: Esteban Morales
+* Author URI: http://forum.cockos.com/member.php?u=133640 
+--]] 
+--====================================================================== 
+local info   = debug.getinfo(1,'S');
+script_path  = info.source:match[[^@?(.*[\/])[^\/]-$]]
+dofile(script_path .. "Nabla Looper Arranged Takes (Delete no prompting).dat")
+
