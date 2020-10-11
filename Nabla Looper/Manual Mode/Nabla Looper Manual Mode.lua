873a1bd2aacd2e8b968c2e81240e637e36aa5c93
Esteban Morales <esteban@nablatools.com>
2020-10-11 12:43:02 -0500

    Update

--- Nabla Looper/Manual Mode/Nabla Looper Manual Mode.lua
+++ Nabla Looper/Manual Mode/Nabla Looper Manual Mode.lua
@@ -1,11 +1,15 @@
 --====================================================================== 
 --[[ 
 * ReaScript Name: Nabla Looper Manual Mode
-* Version: 0.2.0
+* Version: 0.2.2
 * Author: Esteban Morales
 * Author URI: http://forum.cockos.com/member.php?u=133640 
 --]] 
 --====================================================================== 
+
+console = 0
+
+------------------------------------------------------------------
 local info   = debug.getinfo(1,'S');
 script_path  = info.source:match[[^@?(.*[\/])[^\/]-$]]
 dofile(script_path .. "Nabla Looper Manual Mode.dat")
--- Nabla Looper/Manual Mode/Nabla Looper Manual Trigger Actions.lua
+++ Nabla Looper/Manual Mode/Nabla Looper Manual Trigger Actions.lua
@@ -1,13 +1,14 @@
 --====================================================================== 
 --[[ 
 * ReaScript Name: Nabla Looper Manual Trigger Actions
-* Version: 0.2.0
+* Version: 0.2.2
 * Author: Esteban Morales
 * Author URI: https://forum.cockos.com/member.php?u=133640
 --]] 
 --====================================================================== 
+console = 0
+
 local info   = debug.getinfo(1,'S');
 script_path  = info.source:match[[^@?(.*[\/])[^\/]-$]]
 dofile(script_path .. "Nabla Looper Manual Trigger Actions.dat")
-
 
