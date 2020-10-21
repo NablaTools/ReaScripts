88b5d71ee2ab2ebdb200bbb58980e0e0da6ba8b9
Esteban Morales <esteban@nablatools.com>
2020-10-21 11:20:20 -0500

    Update Take System Manual Mode

---
+++ Nabla Looper/Manual Mode/Nabla Looper Manual Take (Start_Stop Recording).lua
@@ -0,0 +1,17 @@
+--====================================================================== 
+--[[ 
+* ReaScript Name: Nabla Looper Manual Mode
+* Version: 0.2.3
+* Author: Esteban Morales
+* Author URI: http://forum.cockos.com/member.php?u=133640 
+--]] 
+--====================================================================== 
+
+console = 0
+
+------------------------------------------------------------------
+local info   = debug.getinfo(1,'S');
+script_path  = info.source:match[[^@?(.*[\/])[^\/]-$]]
+dofile(script_path .. "Nabla Looper Manual Take (Start_Stop Recording.dat")
+
+
